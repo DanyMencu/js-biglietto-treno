e9f6cc3144494077036e94469514a867e0e85299
DanyMencu <dany69men@gmail.com>
2021-10-21 15:53:13 +0200

    Shortened function

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ const age = parseInt( prompt("Inserire la propria età.") );
 const distance = prompt("Quanti Km percorrerai?");
 //3. Calcolare il costo del biglietto
 let price = 0.21 * distance;
-price = price.toFixed(2);
 //4. Applicare uno sconto se è under 18-20%
 if(age < 18) {
-    price = price - ( (price / 100) * 20);
+    price -= ( (price / 100) * 20);
 }
 //5. Applicare uno sconto se è over 65-40%
 if(age >= 65) {
-    price = price - ( (price / 100) * 40);
+    price -= ( (price / 100) * 40);
 }
 //6. OUTPUT del risultato del calcolo
+price = price.toFixed(2);
 document.getElementById("risultato").innerHTML = "Il costo del tuo biglietto sarà:" + price;
