14242ad63417ba58f11d4591c284017710afc2e0
DanyMencu <dany69men@gmail.com>
2021-10-21 14:03:08 +0200

    Project draft create

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//1. Chiedere l'eta dell'utente
+
+//2. Chiedere quanti km percorrerà
+
+//3. Calcolare il prezzo del biglietto
+
+//4. Applicare uno sconto se è under 18
+
+//5. Applicare uno sconto se è under 65
+
+//6. OUTPUT del risultato del calcolo
